8cebd060abadec2ae674e56dc858eb856b52ab91
Thomas Hoddinott <thomashoddinott477@gmail.com>
2020-10-16 10:31:42 +0100

    fixed todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-//todo
-// - polish
-// - improve/fix evaluate - needs to handle division, etc.
-
-// readme: 
-// DIY:
-// npx create-react-app calculator-with-react-hooks
-// codealong: https://www.youtube.com/watch?v=efhYDA8Hsk4
-
 import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [data, setData] = useState("");
+  const [stmt, setStmt] = useState("");
   const calcBtns = [];
   [9,8,7,6,5,4,3,2,1,0,".","%"].forEach(item => {
+    //can we do anything with "%" ?
     calcBtns.push(
-      <button onClick={e => {
-        setData(data + e.target.value)
-      }}
-      value={item}
-      key={item}>
+      <button 
+        onClick={e => {setStmt(stmt + e.target.value)}}
+        value={item}
+        key={item}>
         {item}
       </button>
     )
@@ -27,50 +18,33 @@ function App() {
 
   return (
     <div className="wrapper">
-      <div className="show-input">
-        {data}
-      </div>
-      <div className="digits flex">
-        {calcBtns}
-      </div>
+      <div className="show-input">{stmt}</div>
+      <div className="digits flex">{calcBtns}</div>
       <div className="modifiers subgrid">
-        <button onClick={() => setData(data.substr(0, data.length -1 ))}>
-          Clear
-        </button>
-        <button onClick={() => setData("")}>
-          AC
-        </button>
+        <button onClick={() => setStmt(stmt.substr(0, stmt.length -1 ))}>Clear</button>
+        <button onClick={() => setStmt("")}>AC</button>
       </div>
       <div className="operations subgrid">
-        <button onClick={e => setData(data + e.target.value)} value = "+">
-        +
-        </button>
-        <button onClick={e => setData(data + e.target.value)} value = "-">
-        -
-        </button>
-        <button onClick={e => setData(data + e.target.value)} value = "*">
-        *
-        </button>
-        <button onClick={e => setData(data + e.target.value)} value = "/">
-        /
-        </button>
-
+        <button onClick={e => setStmt(stmt + e.target.value)} value = "+">+</button>
+        <button onClick={e => setStmt(stmt + e.target.value)} value = "-">-</button>
+        <button onClick={e => setStmt(stmt + e.target.value)} value = "*">*</button>
+        <button onClick={e => setStmt(stmt + e.target.value)} value = "/">/</button>
         <button 
           onClick={e => {
             try {
-              setData(
-                String(eval(data)).length > 3 &&
-                  String(eval(data)).includes(".")
-                    ? String(eval(data)).toFixed(4)
-                    : String(eval(data))
+              const eval_stmt = eval(stmt)
+              setStmt(
+                String(eval_stmt).length > 3 &&
+                  String(eval_stmt).includes(".")
+                    ? String(eval_stmt.toFixed(3)) //limit dp to 3
+                    : String(eval_stmt)
               )
             } catch(err) {
               alert(err)
             }
           }}
           value = "="   
-        >
-        =</button>
+        >=</button>
       </div>
     </div>
   );
