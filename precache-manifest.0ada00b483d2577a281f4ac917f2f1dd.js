166278aa97c4af760f3e94d9c6393a0c8f9c9409
Thomas Hoddinott <thomashoddinott477@gmail.com>
2020-10-16 13:51:54 +0100

    Updates

---
+++ precache-manifest.0ada00b483d2577a281f4ac917f2f1dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3e15a2cc5000e3dfffe70f77692cc0",
+    "url": "/calculator-with-react-hooks/index.html"
+  },
+  {
+    "revision": "04fa6c1b97175655b968",
+    "url": "/calculator-with-react-hooks/static/css/main.7c5aefb2.chunk.css"
+  },
+  {
+    "revision": "b381199c00f961ba7056",
+    "url": "/calculator-with-react-hooks/static/js/2.e7e557cc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/calculator-with-react-hooks/static/js/2.e7e557cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04fa6c1b97175655b968",
+    "url": "/calculator-with-react-hooks/static/js/main.bd0427a9.chunk.js"
+  },
+  {
+    "revision": "e8d5942ae7aac432ac3c",
+    "url": "/calculator-with-react-hooks/static/js/runtime-main.b2ebe37e.js"
+  }
+]);
